f610cfcc8504688f227b581224306d0f892ec7b2
Fabian Feichter <ffeichter@eurofunk.com>
2024-12-17 20:25:38 +0100

    Try out Spring (Boot) behaviour #8

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,9 +2,13 @@ package com.example.demo;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class DemoApplicationTests {
+
+    @MockBean
+    private Foo foo1;
 
     @Test
     void contextLoads() {
